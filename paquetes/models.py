fee587eeb5ea085f9b072c7b63b8bcdc8bfecc34
Angel Antezana <ghehahanghel@gmail.com>
2019-07-05 04:51:40 -0400

    Creando modelo

--- paquetes/models.py
+++ paquetes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Paquete(models.Model):
+    destino = models.CharField(max_length=100, verbose_name="Destino")
+    descripcion = models.TextField(verbose_name="Descripcion")
+    precio = models.CharField(max_length=100, verbose_name="Precio")
+    guia = models.CharField(max_length=100, verbose_name="Guia")
+    fechaPartida = models.CharField(max_length=100, verbose_name="FechaPartida")
+
+    def __str__(self):
+        return self.name
