542e7b636ac242e504b6fc39e5ddd2c1f6141697
Angel Antezana <ghehahanghel@gmail.com>
2019-07-05 05:40:53 -0400

    registrando rutas

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 
 from django.contrib import admin
 
+from rest_framework import routers
+
+from paquetes import views
+
 admin.autodiscover()
 
 
@@ -12,8 +16,10 @@ admin.autodiscover()
 # path('blog/', blog.urls, name="blog")
 #
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
-print('testingi automatic deploy')
+router = routers.DefaultRouter()
+router.register('paquetes', views.PaqueteViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/', include(router.urls)),
 ]
